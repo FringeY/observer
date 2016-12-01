07f2d63fb9eda401be5b13e8e1f90f410c64e4bc
yaorui <465964690@qq.com>
2016-12-01 14:21:47 +0800

    add redis

---
+++ test.js
@@ -0,0 +1,8 @@
+op - 23:49:41 up 19 days, 23:30,  2 users,  load average: 0.04, 0.13, 0.12
+Tasks:   1 total,   0 running,   1 sleeping,   0 stopped,   0 zombie
+%Cpu(s):  1.0 us,  0.3 sy,  0.0 ni, 98.7 id,  0.0 wa,  0.0 hi,  0.0 si,  0.0 st
+KiB Mem:   1017712 total,   808016 used,   209696 free,    19516 buffers
+KiB Swap:        0 total,        0 used,        0 free.   612860 cached Mem
+
+  PID USER      PR  NI    VIRT    RES    SHR S %CPU %MEM     TIME+ COMMAND
+    1 root      20   0   33464   2636   1332 S  0.0  0.3   0:16.10 init
--- watch.js
+++ watch.js
@@ -1,10 +1,23 @@
 // 执行 top 命令获取系统信息，默认为头5行
 const exec = require('child_process').exec;
+const redis = require('redis');
+const client = redis.createClient({detect_buffers: true});
+
+client.on("error", function (err) {
+    console.log("Error " + err);
+});
 
 const top = exec('top | head -5', {});
 
 top.stdout.on('data', function (data) {
-  console.log(data);
+  client.set('sysinfo', data);
+  client.get('sysinfo', function (err, reply) {
+    if (err !== null) {
+      console.log(err);
+    } else {
+      console.log(reply.toString());
+    }
+  });
 });
 
 top.stderr.on('data', function (data) {
@@ -12,5 +25,6 @@ top.stderr.on('data', function (data) {
 })
 
 top.on('exit', function (code) {
+  client.quit();
   console.log('child process exited with code ' + code);
 })
