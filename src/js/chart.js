bae47ef41f29066c2c408d29e5d99f9a037d316a
yaorui <465964690@qq.com>
2016-12-02 09:09:47 +0800

    debug

--- src/js/chart.js
+++ src/js/chart.js
@@ -214,22 +214,22 @@ window.onload = function () {
   socket.on('sysInfo', function (data) {
     console.log(data);
     sysInfo = data.data;
-    var time = sysInfo.match(/top\s\-\s(\d+\:\d+\:\d+)/)[1];
+    var time = sysInfo.match(/top\s*\-\s*(\d+\:\d+\:\d+)/)[1];
     Times.push(time);
     showTime(time);
-    var uptime = sysInfo.match(/up\s(\d+\sdays,\s\d+\:\d+)/).slice(1);
+    var uptime = sysInfo.match(/up\s*(\d+\s*days,\s*\d+\:\d+)/).slice(1);
     showUpTime(uptime);
-    var loadaverage = sysInfo.match(/load\saverage\:\s(\d+\.\d+),\s(\d+\.\d+),\s(\d+\.\d+)/).slice(1);
+    var loadaverage = sysInfo.match(/load\s*average\:\s*(\d+\.\d+),\s*(\d+\.\d+),\s*(\d+\.\d+)/).slice(1);
     showLoadAverage(loadaverage);
-    var tasks = sysInfo.match(/Tasks\:\s*(\d+)\stotal,\s*(\d+)\srunning,\s*(\d+)\ssleeping,\s*(\d+)\sstopped,\s*(\d+)\szombie/).slice(1)
+    var tasks = sysInfo.match(/Tasks\:\s*(\d+)\s*total,\s*(\d+)\s*running,\s*(\d+)\s*sleeping,\s*(\d+)\s*stopped,\s*(\d+)\szombie/).slice(1)
     showTasks(tasks);
-    var cpu = sysInfo.match(/Cpu\(s\):\s*(\d+\.\d+)\sus,\s*(\d+\.\d+)\ssy,\s*(\d+\.\d+)\sni,\s*(\d+\.\d+)\sid,\s*(\d+\.\d+)\swa,\s*(\d+\.\d+)\shi,\s*(\d+\.\d+)\ssi,\s*(\d+\.\d+)\sst/).slice(1);
+    var cpu = sysInfo.match(/Cpu\(s\):\s*(\d+\.\d+)\s*us,\s*(\d+\.\d+)\s*sy,\s*(\d+\.\d+)\s*ni,\s*(\d+\.\d+)\s*id,\s*(\d+\.\d+)\s*wa,\s*(\d+\.\d+)\s*hi,\s*(\d+\.\d+)\s*si,\s*(\d+\.\d+)\s*st/).slice(1);
     parseCpu(cpu);
-    var mem = sysInfo.match(/KiB\sMem\:\s*(\d+)\stotal,\s*(\d+)\sused,\s*(\d+)\sfree,\s*(\d+)\sbuffers/).slice(1);
+    var mem = sysInfo.match(/KiB\s*Mem\:\s*(\d+)\s*total,\s*(\d+)\s*used,\s*(\d+)\s*free,\s*(\d+)\s*buffers/).slice(1);
     parseMem(mem);
     // var swap = sysInfo.match(/KiB\sSwap\:\s*(\d+)\stotal,\s*(\d+)\sused,\s*(\d+)\sfree/).slice(1);
     // Swaps.push(swap);
-    cachedMem = sysInfo.match(/(\d+)\scached\sMem/)[1];
+    cachedMem = sysInfo.match(/(\d+)\s*cached\s*Mem/)[1];
     parseCache(cachedMem);
 
     setTimeout(function () {
